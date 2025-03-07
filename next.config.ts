046b55b8773b54008964f9beaab7ef35f160252d
enric <cirnemiro@gmail.com>
2025-03-07 12:33:07 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
